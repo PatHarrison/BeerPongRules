60efb9c2b159b6fb311cc532cdb553388018270c
Tarang74 <tjanawalkar74@gmail.com>
2021-07-10 03:07:08 +1000

    Fixed make.bat Fixed quotation mark errors

--- make.bat
+++ make.bat
@@ -2,31 +2,43 @@
 
 @ECHO OFF
 
-IF %1.==. GOTO build
-IF %1%=="-h" IF %1%=="-help" GOTO help
+IF "%~1"=="" GOTO :build
 
-:: Help
-:help
-    ECHO Compiles BPRules.tex using PDFLaTeX.
-    ECHO.
-    ECHO make.bat [-h ^| -help]     Provides help information.
-    ECHO make.bat [-o ^| -open]     Open the PDF file after compilation.
-    ECHO.
-    GOTO end
+IF "%1"=="-h" GOTO :help
+IF "%1"=="-help" GOTO :help
+IF NOT "%~1"=="" GOTO :invalid
 
 :: Build PDF
 :build
     ECHO -- Building PDF --
     pdflatex -quiet -output-directory=Output BPRules.tex
+    pdflatex -quiet -output-directory=Output BPRules.tex
 
-IF %1.==. GOTO end
-IF %1=="-o" IF %1=="-open" GOTO openPDF
+IF "%~1"=="" GOTO :EOF
+
+IF "%1"=="-o" GOTO :openPDF
+IF "%1"=="-open" GOTO :openPDF
+
+IF NOT "%~1"=="" GOTO :EOF
 
 :: Open PDF
 :openPDF
     ECHO -- Opening PDF --
     Output\BPRules.pdf
-    GOTO end
+    GOTO :EOF
 
-:end
+:: Help
+:help
+    ECHO %~nx0 compiles BPRules.tex using PDFLaTeX.
+    ECHO.
+    ECHO     Usage: %~nx0 [option]
+    ECHO.
+    ECHO %~nx0 [-h ^| -help]     Provides help information.
+    ECHO %~nx0 [-o ^| -open]     Open the PDF after compilation.
+    GOTO :EOF
 
+:: Invalid Parameter
+:invalid
+    ECHO Unknown option: %1
+    ECHO Try "%~nx0 -h" for more information.
+    GOTO :EOF
