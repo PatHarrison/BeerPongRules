4a1c1b976c70256225b8ce823df38ce8359808db
Tarang74 <tjanawalkar74@gmail.com>
2021-07-10 19:27:17 +1000

    Added verbose option to batch file.

--- make.bat
+++ make.bat
@@ -2,35 +2,78 @@
 
 @ECHO OFF
 
+setlocal ENABLEDELAYEDEXPANSION
+
+@REM Set global variables
+SET filename=%~nx0
+
+SET /A verbose=0
+SET /A open=0
+
 @REM If no parameters, build without opening
 IF "%~1"=="" GOTO :default
-
-@REM Build and open PDF
-IF "%~1"=="-o" GOTO :open
-IF "%~1"=="-open" GOTO :open
 
 @REM Display help text
 IF "%~1"=="-h" GOTO :help
 IF "%~1"=="-help" GOTO :help
 
-@REM Parameter is invalid
-GOTO :invalid
+@REM Loop through all parameters
+SET /A numP=0
+:loop
+    @REM Use numP to test each parameter for validity
+    SET numP=0
+
+    @REM Check options
+    IF "%1"=="-v" (
+        SET numP=1
+        SET verbose=1
+    ) ELSE (
+        IF "%1"=="-verbose" (
+            SET numP=1
+            SET verbose=1
+        )
+        IF "%1"=="-o" (
+            SET numP=1
+            SET open=1
+        )
+        IF "%1"=="-open" (
+            SET numP=1
+            SET open=1
+        )
+    )
+
+    @REM If current option is not valid
+    IF !numP!==0 GOTO :invalid
+    
+    @REM Shift variables and loop if another option exists
+    SHIFT
+    IF NOT "%1"=="" GOTO :loop
 
 :: Default
 :default
-    CALL :buildPDF
-    GOTO :EOF
+    @REM Verbose options
+    IF %verbose%==0 CALL :buildPDF
+    IF %verbose%==1 CALL :buildPDFv
+    
+    @REM Open options
+    IF %open%==0 GOTO :EOF
+    IF %open%==1 CALL :openPDF
 
-:: Open
-:open
-    CALL :buildPDF
-    CALL :openPDF
+    @REM Finally:
     GOTO :EOF
 
 :: Build PDF
 :buildPDF
     ECHO -- Building PDF --
     pdflatex -quiet -output-directory=Output BPRules.tex
+    pdflatex -quiet -output-directory=Output BPRules.tex
+    EXIT /B
+
+:: Build PDF verbose
+:buildPDFv
+    ECHO -- Building PDF --
+    pdflatex -output-directory=Output BPRules.tex
+    pdflatex -output-directory=Output BPRules.tex
     EXIT /B
 
 :: Open PDF
@@ -41,16 +84,18 @@ GOTO :invalid
 
 :: Help
 :help
-    ECHO %~nx0 compiles BPRules.tex using PDFLaTeX.
+    ECHO %filename% compiles BPRules.tex using PDFLaTeX.
     ECHO.
-    ECHO     Usage: %~nx0 [option]
+    ECHO     Usage: %filename% [option] ...
     ECHO.
-    ECHO %~nx0 [-h ^| -help]     Provides help information.
-    ECHO %~nx0 [-o ^| -open]     Open the PDF after compilation.
+    ECHO Available options:
+    ECHO     %filename% [-h ^| -help]       Provides help information.
+    ECHO     %filename% [-v ^| -verbose]    Show compiler output.
+    ECHO     %filename% [-o ^| -open]       Open the PDF after compilation.
     GOTO :EOF
 
 :: Invalid Parameter
 :invalid
-    ECHO Unknown option: %~1
-    ECHO Try "%~nx0 -h" for more information.
+    ECHO Unknown option: %1
+    ECHO Try "%filename% -h" for more information.
     GOTO :EOF
